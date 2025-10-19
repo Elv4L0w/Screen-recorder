1eb9ccf623a88ca3aa1c404f145a9c49f2d6f30d
Elv4L0w <91699930+Elv4L0w@users.noreply.github.com>
2025-10-20 01:24:46 +0200

    made use of it because of newer electron version

--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,12 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  getSources: () => ipcRenderer.invoke('get-sources'),
+  showMenu: (template) => ipcRenderer.invoke('show-menu', template),
+  onMenuItemClicked: (callback) => ipcRenderer.on('menu-item-clicked', (event, label) => callback(label))
+});
+
+
